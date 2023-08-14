1daeb1095de53adcfb3c6dc5575c8d727d04d7ad
Mateus Moura <mateuspragana@gmail.com>
2023-08-14 09:45:55 -0300

    feat: configurando background color padrão

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -5,6 +5,19 @@ import { themes } from "@storybook/theming"
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#f8f8f8',
+        },
+        {
+          name: 'dark',
+          value: '#333333',
+        }
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
