96d75c810dd6b85afd0133b6cf9e9ca46f933c47
Mateus Moura <mateuspragana@gmail.com>
2023-08-15 10:43:29 -0300

    feat: criando stories do Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/MouraPragana.png',
+    alt: 'Mateus',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
