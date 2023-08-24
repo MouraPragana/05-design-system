671fb6590f600b5178f296962c801949b6632771
Mateus Moura <mateuspragana@gmail.com>
2023-08-24 09:29:33 -0300

    feat: configurando seu control e seu displayname

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,32 @@ export default {
   component: Text,
 
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere blanditiis dignissimos quis nemo, a, minima repellendus vitae vero maxime cumque nesciunt quia perspiciatis earum nulla laboriosam illo itaque explicabo sunt.',
+  },
+
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
